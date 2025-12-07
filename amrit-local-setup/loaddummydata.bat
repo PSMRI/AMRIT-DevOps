84e609afb882a61b4447dc179e75083d12722fb0
ramnar <raam.soft@gmail.com>
2025-12-07 14:23:36 +0530

    updated windows script

--- amrit-local-setup/loaddummydata.bat
+++ amrit-local-setup/loaddummydata.bat
@@ -6,8 +6,16 @@ set PORT=3306
 set USER=root
 set PASSWORD=1234
 
+:: 1. Download the file using PowerShell's Invoke-WebRequest (equivalent to wget)
+echo Downloading AmritMasterData.zip...
+powershell -Command "Invoke-WebRequest -Uri 'https://1865391384-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FYfDZFIsUuulWkRHaq4c1%2Fuploads%2F1WdSAf0fQBeJOea70EXE%2FAmritMasterData.zip?alt=media&token=18e0b6d6-487c-4c0c-967a-02cdd94d61ad' -OutFile 'AmritMasterData.zip'"
+
+:: 2. Extract the file using PowerShell's Expand-Archive (equivalent to unzip)
+echo Extracting AmritMasterData.zip...
+powershell -Command "Expand-Archive -Path 'AmritMasterData.zip' -DestinationPath 'AmritMasterDataFiles'"
+
 :: Path to the extracted SQL files
-set SQL_DIR=<PATH to the extracted files directory>
+set SQL_DIR="AmritMasterDataFiles/AmritMasterData"
 
 :: Associating files with databases
 set DATABASES="AmritMasterData.sql=db_iemr" "m_beneficiaryregidmapping_dump_1097.sql=db_1097_identity" "m_beneficiaryregidmapping_dump.sql=db_identity"
@@ -30,3 +38,8 @@ for %%F in (%DATABASES%) do (
 )
 
 pause
+
+# Clean up the extracted files
+del AmritMasterData.zip
+rmdir /s /q AmritMasterDataFiles
+echo Cleaned up AmritMasterDataFiles folder.
