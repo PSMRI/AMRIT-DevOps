f3d923fccba950cd24ecde4ce6fd580ddfa3fce3
ramnar <raam.soft@gmail.com>
2026-02-28 09:08:36 +0530

    updated script

--- Applications/Common-Platform/start-common.sh
+++ Applications/Common-Platform/start-common.sh
@@ -1,22 +1,38 @@
+
 #!/bin/bash
 set -e
 
-AMRIT_HOME="/home/ramnar/Documents/amrit"
+# Set AMRIT_HOME robustly to the parent directory of this script
+AMRIT_HOME="$(dirname $(dirname $(realpath $0)))"
 cd "$AMRIT_HOME"
 
-# clone git repository for Common-API and Common-UI if not already done
-if [ ! -d "Common-API" ]; then
-    git clone https://github.com/PSMRI/Common-API.git
-    cd "$AMRIT_HOME"/Common-API
+echo "[INFO] AMRIT_HOME set to $AMRIT_HOME"
+
+# Clone Common-API if not present
+if [ ! -d "$AMRIT_HOME/Common-API" ]; then
+    echo "[INFO] Cloning Common-API repository..."
+    git clone https://github.com/PSMRI/Common-API.git "$AMRIT_HOME/Common-API"
+    cd "$AMRIT_HOME/Common-API"
     cp src/main/environment/common_example.properties src/main/environment/common_local.properties
+    echo "[INFO] Common-API environment files copied."
+    cd "$AMRIT_HOME"
+else
+    echo "[INFO] Common-API already exists."
 fi
-if [ ! -d "Common-UI" ]; then
-    git clone https://github.com/PSMRI/Common-UI.git
+
+# Clone Common-UI if not present
+if [ ! -d "$AMRIT_HOME/Common-UI" ]; then
+    echo "[INFO] Cloning Common-UI repository..."
+    git clone https://github.com/PSMRI/Common-UI.git "$AMRIT_HOME/Common-UI"
+    cd "$AMRIT_HOME"
+else
+    echo "[INFO] Common-UI already exists."
 fi
 
+# Start local setup containers
+echo "[INFO] Starting local setup containers..."
+gnome-terminal -- bash -c "cd \"$AMRIT_HOME/AMRIT-DevOps/amrit-local-setup\";sudo docker-compose up -d; exec bash"
 
-gnome-terminal -- bash -c "cd "$AMRIT_HOME"/AMRIT-DevOps/amrit-local-setup;sudo docker-compose up -d"
-
-# start schema management service only first time
-
-gnome-terminal -- bash -c "cd "$AMRIT_HOME"/Common-API;mvn clean install -DskipTests=true; mvn spring-boot:run -DENV_VAR=local; exec bash"
+# Start Common-API service
+echo "[INFO] Starting Common-API service..."
+gnome-terminal -- bash -c "cd \"$AMRIT_HOME/Common-API\";mvn clean install -DskipTests=true; mvn spring-boot:run -DENV_VAR=local; exec bash"
--- Applications/ECD/start.sh
+++ Applications/ECD/start.sh
@@ -1,23 +1,46 @@
+
 #!/bin/bash
 set -e
 
+# Set AMRIT_HOME robustly to the parent directory of this script
+AMRIT_HOME="$(dirname $(dirname $(realpath $0)))"
+cd "$AMRIT_HOME"
+echo "[INFO] AMRIT_HOME set to $AMRIT_HOME"
+
+# Start common services
+echo "[INFO] Starting common services..."
 start-common.sh
 
-# clone git repository for ECD-API and ECD-UI if not already done
-if [ ! -d "ECD-API" ]; then
-    git clone https://github.com/PSMRI/ECD-API.git
-    cd "$AMRIT_HOME"/ECD-API
+# Clone ECD-API if not present
+if [ ! -d "$AMRIT_HOME/ECD-API" ]; then
+    echo "[INFO] Cloning ECD-API repository..."
+    git clone https://github.com/PSMRI/ECD-API.git "$AMRIT_HOME/ECD-API"
+    cd "$AMRIT_HOME/ECD-API"
     cp src/main/environment/ecd_example.properties src/main/environment/ecd_local.properties
+    echo "[INFO] ECD-API environment files copied."
+    cd "$AMRIT_HOME"
+else
+    echo "[INFO] ECD-API already exists."
 fi
-if [ ! -d "ECD-UI" ]; then
-    git clone https://github.com/PSMRI/ECD-UI.git
-    cd "$AMRIT_HOME"/ECD-UI
+
+# Clone ECD-UI if not present
+if [ ! -d "$AMRIT_HOME/ECD-UI" ]; then
+    echo "[INFO] Cloning ECD-UI repository..."
+    git clone https://github.com/PSMRI/ECD-UI.git "$AMRIT_HOME/ECD-UI"
+    cd "$AMRIT_HOME/ECD-UI"
     sudo npm install --legacy-peer-deps
     git submodule update --init --recursive
     cp src/environments/environment.local.ts src/environments/environment.ts
+    echo "[INFO] ECD-UI dependencies installed and environment files copied."
+    cd "$AMRIT_HOME"
+else
+    echo "[INFO] ECD-UI already exists."
 fi
 
+# Start ECD-API service
+echo "[INFO] Starting ECD-API service..."
+gnome-terminal -- bash -c "cd \"$AMRIT_HOME/ECD-API\";mvn clean install -DskipTests=true; mvn spring-boot:run -DENV_VAR=local; exec bash"
 
-gnome-terminal -- bash -c "cd "$AMRIT_HOME"/ECD-API;mvn clean install -DskipTests=true; mvn spring-boot:run -DENV_VAR=local; exec bash"
-
-gnome-terminal -- bash -c "cd "$AMRIT_HOME"/ECD-UI;ng serve; exec bash"
+# Start ECD-UI service
+echo "[INFO] Starting ECD-UI service..."
+gnome-terminal -- bash -c "cd \"$AMRIT_HOME/ECD-UI\";ng serve; exec bash"
